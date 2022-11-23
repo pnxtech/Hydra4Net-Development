646ba020d2a670f31b14ccc3660f11cb17ad4ff3
Carlos Justiniano <cjus@ieee.org>
2022-11-23 14:27:28 -0700

    Refactoring

--- Hydra4NET/Hydra.cs
+++ Hydra4NET/Hydra.cs
@@ -2,9 +2,60 @@
 {
     public class Hydra
     {
-        public void init()
-        {
+        private Task? _internalTask = null;
+        private readonly PeriodicTimer _timer;
+        private int _secondsTick = 1;
+        private readonly CancellationTokenSource _cts = new();
 
+        public Hydra()
+        {
+            TimeSpan interval = TimeSpan.FromSeconds(1);
+            _timer = new PeriodicTimer(interval);
+        }
+
+        public void Init()
+        {
+            _internalTask = UpdatePresence();
+        }
+
+        private async Task UpdatePresence()
+        {
+            try 
+            { 
+                while (await _timer.WaitForNextTickAsync(_cts.Token))
+                {
+                    await PresenceEvent();
+                    if (_secondsTick++ == 3)
+                    {
+                        await HealthCheckEvent();
+                        _secondsTick = 1;
+                    }
+                }
+            }
+            catch (OperationCanceledException) 
+            {
+            }
+        }
+
+        private async Task PresenceEvent()
+        {
+            Console.WriteLine("Handling Update Presence");
+            await Task.Delay(100);
+        }
+
+        private async Task HealthCheckEvent()
+        {
+            Console.WriteLine("Handling Update Health");
+            await Task.Delay(100);
+        }
+
+        public async Task Shutdown() { 
+            if (_internalTask != null)
+            {
+                _cts.Cancel();
+                await _internalTask;
+                _cts.Dispose();
+            }
         }
     }
 }
---
+++ TestRig/Program.cs
@@ -0,0 +1,8 @@
+﻿using Hydra4NET;
+
+Hydra hydra = new Hydra();
+hydra.Init();
+
+Console.WriteLine("Press any key to shutdown");
+Console.ReadKey();
+await hydra.Shutdown();
