63e834fd53a09d07c2fb6d7e7ca5ea188b380667
Carlos Justiniano <cjus@ieee.org>
2022-11-24 11:56:38 -0700

    Cleanup config in progress

--- Hydra4NET/Config.cs
+++ Hydra4NET/Config.cs
@@ -1,10 +1,10 @@
-using ConfigJson;
+using Hydra4NET.ConfigJson;
 
 namespace Hydra4NET
 {
     public class Config
     {
-        pubic Load(string configPath)
+        public void Load(string configPath)
         {
             string json = File.ReadAllText(configPath);
             Rootobject config = JsonConvert.DeserializeObject<Rootobject>(json);
