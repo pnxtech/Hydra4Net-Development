644f49295c7e61b93f8f5194e3f2dc8056706596
cjus <Carlos.Justiniano@gmail.com>
2022-12-30 11:26:44 -0700

    fixed formatting issue

--- MessageDemo/Sender.cs
+++ MessageDemo/Sender.cs
@@ -3,43 +3,43 @@ using Hydra4NET;
 
 namespace MessageDemo
 {
-	/**
-		* MessageBody
-		* This class represents the body type of a UMF message body's entry
-		*/
-	public class MessageBody
-	{
-			public string? Msg { get; set; }
-			public int? Id { get; set; }
-	}
+    /**
+	* MessageBody
+	* This class represents the body type of a UMF message body's entry
+	*/
+    public class MessageBody
+    {
+        public string? Msg { get; set; }
+        public int? Id { get; set; }
+    }
 
-	/**
-		* Message
-		* This class represents a UMF class with a body type of MessageBody.
-		* See the CreateUMFMessage below for an example of how the body is set.
-		*/
-	public class Message: UMF<MessageBody>
-	{
-			public TestMsg()
-			{
-					To = "queuer-svcs:/";
-					Frm = "sender-svcs:/";
-					Typ = "testMsg";
-			}
-	}
+    /**
+	 * Message
+	 * This class represents a UMF class with a body type of MessageBody.
+	 * See the CreateUMFMessage below for an example of how the body is set.
+	 */
+    public class Message : UMF<MessageBody>
+    {
+        public Message()
+        {
+            To = "queuer-svcs:/";
+            Frm = "sender-svcs:/";
+            Typ = "sender";
+        }
+    }
 
-	public class Sender
-	{
-		private Hydra _hydra;
+    public class Sender
+    {
+        private Hydra _hydra;
 
-		public Sender(Hydra hydra)
-		{
-			_hydra = hydra;
-		}
+        public Sender(Hydra hydra)
+        {
+            _hydra = hydra;
+        }
 
-		public void ProcessMessage(string type, string? message)
-		{
-		}
-	}
+        public void ProcessMessage(string type, string? message)
+        {
+        }
+    }
 }
 
